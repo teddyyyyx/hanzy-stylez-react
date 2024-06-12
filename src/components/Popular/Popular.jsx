826f1ec4528e2820ed4816c40cfd0a2f599fde17
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-13 00:23:37 +0800

    Revised UI for Popular component

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -31,7 +31,7 @@ export const Popular = () => {
 
         <div className="popular-items" ref={scrollContainerRef}>
           {data_product.map((item, i) => {
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />;
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} category={item.category}/>;
           })}
         </div>
 
