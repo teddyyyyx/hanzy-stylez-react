b31f755beb6518b63d2dba613d895d238b9ff3bd
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-13 00:20:45 +0800

    Fixed undefined 'category' bug in routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import React from 'react'
+import React from 'react';
 import { Navbar } from './components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { ShopCategory } from './Pages/ShopCategory';
 import { Product } from './Pages/Product';
 import { Cart } from './Pages/Cart';
 import { LoginSignup } from './Pages/LoginSignup';
 import { Shop } from './Pages/Shop';
 import { Footer } from './components/Footer/Footer';
-import men_banner from './components/Assets/banner_mens.png'
-import women_banner from './components/Assets/banner_women.png'
-import kid_banner from './components/Assets/banner_kids.png'
+import men_banner from './components/Assets/banner_mens.png';
+import women_banner from './components/Assets/banner_women.png';
+import kid_banner from './components/Assets/banner_kids.png';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import { NewsLetter } from './components/NewsLetter/NewsLetter';
+
+const MainContent = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop />} />
+        <Route path='/men' element={<ShopCategory banner={men_banner} category="men" />} />
+        <Route path='/women' element={<ShopCategory banner={women_banner} category="women" />} />
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kids" />} />
+        <Route path='/:category/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<LoginSignup />} />
+      </Routes>
+
+
+        {/*Render the NewsLetter component only if the current URL path is not /login. */}
+      {location.pathname !== '/login' && <NewsLetter />}   
+      <Footer />            
+    </>
+  );
+};
 
 const App = () => {
   return (
     <div className='App'>
-        <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<Shop/>}/>
-                <Route path='/mens' element={<ShopCategory banner={men_banner} category="mens"/>} />
-                <Route path='/womens' element={<ShopCategory banner={women_banner} category="womens"/>} />
-                <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kids"/>}/>
-             
-                <Route path="/product" element={<Product/>}>
-                    <Route path=":productId" element={<Product />}/>
-                </Route>
-
-                <Route path='/cart' element={<Cart />}/>
-
-                <Route path='login' element={<LoginSignup />}/>
-            </Routes>
-            <Footer />
-        </BrowserRouter>
+      <BrowserRouter>
+        <MainContent />
+      </BrowserRouter>
     </div>
-)
-}
+  );
+};
 
 export default App;
