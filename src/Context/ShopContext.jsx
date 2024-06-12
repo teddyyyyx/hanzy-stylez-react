d57db8ab143a019fe4b1943fb3cb023560e14de9
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-13 00:22:08 +0800

    Added functions in the context for the cart.

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,9 +1,60 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import all_product from '../components/Assets/all_product';
 export const ShopContext = createContext(null);
 
+
+const getDefaultCart = () => {
+    let cart = {};
+    for(let index = 0; index < all_product.length + 1; index++){
+        // const element = array[index];
+        cart[index] = 0;
+    }
+    return cart;
+}
+
 const ShopContextProvider = (props) => {
-    const contextValue = {all_product};
+    const [cartItems, setCartItems] = useState(getDefaultCart())
+  
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
+        console.log(cartItems)
+    }
+
+    const minusFromCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}))
+    }
+
+    const removeAllCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: 0}));
+        console.log(cartItems)
+    }
+
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0; 
+        for(const item in cartItems)
+            {
+                if(cartItems[item] > 0){
+                    let itemInfo = all_product.find((product) => product.id === Number(item))
+                    totalAmount += itemInfo.new_price * cartItems[item];
+                }
+            }
+            return totalAmount;
+    }
+
+    const getTotalCartItems = () =>{
+        let totalItem = 0;
+        for(const item in cartItems){
+            if(cartItems[item] > 0){
+                totalItem += cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue = {all_product, cartItems, addToCart, getTotalCartAmount, getTotalCartItems, minusFromCart, removeAllCart};
+
+
 
     return (
         <ShopContext.Provider value={contextValue}>
