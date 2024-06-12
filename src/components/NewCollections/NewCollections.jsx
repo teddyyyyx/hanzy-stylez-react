f90b3594623a033df890f5fc319c91ad37832d88
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-13 00:25:27 +0800

    Added responsiveness and improved UI

--- src/components/NewCollections/NewCollections.jsx
+++ src/components/NewCollections/NewCollections.jsx
@@ -6,12 +6,12 @@ import { Item } from '../Item/Item'
 export const NewCollections = () => {
   return (
     <div className='new-collections'>
-        <h1>NEW COLLECTIONS</h1>
+        <h1 className='new-collections-title'>NEW COLLECTIONS</h1>
         <hr />
 
-        <div className='collections'>
+        <div className='collections-container'>
             {new_collections.map((item, i) =>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} category={item.category}/>
             })}
         </div>
     </div>
