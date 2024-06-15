78f5b6ff94b51cbd2cd090dc4729ccf90f484c6e
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-15 16:05:01 +0800

    Added Navlink in 'latest collection'  button

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './intro.css'
+import { Link } from 'react-router-dom'
 import honeyImg2 from '../Assets/honeyImg2.png'
 
 export const Intro = () => {
@@ -10,9 +11,12 @@ export const Intro = () => {
         <hr />
         <hr className='intro-hr'/>
         <h1 className='intro-quote'>Refresh your wardrobe <br /> with  the  latest trends  <br /> off the runaway</h1>
-        <button className='intro-button'>Latest Collection
-          <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#111111"><path d="m702-301-43-42 106-106H120v-60h646L660-615l42-42 178 178-178 178Z"/></svg>
-        </button>
+        
+        <Link className='intro-button' to='/latest' end>
+              <p>Latest Collection</p>
+              <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#111111"><path d="m702-301-43-42 106-106H120v-60h646L660-615l42-42 178 178-178 178Z"/></svg>
+        </Link>
+
       </div>
       <img  src={honeyImg2} alt="" className='honeyImg2'/>
     </section>
