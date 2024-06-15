ffdaeb13552eef163894cafab338526358925d24
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-15 15:59:58 +0800

    Added new '/latest' Route. Updated banner image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navbar } from './components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { ShopCategory } from './Pages/ShopCategory';
@@ -7,15 +7,20 @@ import { Cart } from './Pages/Cart';
 import { LoginSignup } from './Pages/LoginSignup';
 import { Shop } from './Pages/Shop';
 import { Footer } from './components/Footer/Footer';
-import men_banner from './components/Assets/banner_mens.png';
-import women_banner from './components/Assets/banner_women.png';
-import kid_banner from './components/Assets/banner_kids.png';
+import men_banner from './components/Assets/banner-images/banner_mens.png';
+import women_banner from './components/Assets/banner-images/banner_women.png';
+import kid_banner from './components/Assets/banner-images/banner_kids.png';
+import latest_banner from './components/Assets/banner-images/banner_latest.png';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import { NewsLetter } from './components/NewsLetter/NewsLetter';
+import { LatestCollection } from './Pages/LatestCollection';
 
 const MainContent = () => {
   const location = useLocation();
 
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, []);
   return (
     <>
       <ScrollToTop />
@@ -28,6 +33,8 @@ const MainContent = () => {
         <Route path='/:category/product/:productId' element={<Product />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/login' element={<LoginSignup />} />
+
+        <Route path='/latest' element={<LatestCollection banner={latest_banner}/>} />
       </Routes>
 
 
