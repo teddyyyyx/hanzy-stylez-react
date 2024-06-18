47c92078cd33fae87fa2271a6f6b6e1fb53a038b
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-18 10:45:22 +0800

    Minor bug fixes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -50,7 +50,7 @@ const dropdown_toggle = (e) => {
         <div className='nav-login-cart'>
           <NavLink to='/login' end><button>Login</button></NavLink>
           <NavLink to='/login' end>
-            <svg className='login-svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"  fill="#e8eaed"><path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z"/></svg>
+            <svg className='login-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"  fill="#e8eaed"><path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z"/></svg>
           </NavLink>
           
         
